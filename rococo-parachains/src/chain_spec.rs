b0392d9c212149396d2f8ae9b18abbd29dcf595a
Christian Langenbacher <christian.langenbacher91@gmail.com>
2021-02-18 12:05:24 +0100

    remove some trivial diffs to cumulus

--- rococo-parachains/src/chain_spec.rs
+++ rococo-parachains/src/chain_spec.rs
@@ -26,6 +26,7 @@ use sc_service::ChainType;
 use serde::{Deserialize, Serialize};
 use sp_core::{sr25519, Pair, Public};
 use sp_runtime::traits::{IdentifyAccount, Verify};
+
 /// Specialized `ChainSpec` for the normal parachain runtime.
 pub type ChainSpec = sc_service::GenericChainSpec<parachain_runtime::GenesisConfig, Extensions>;
 
