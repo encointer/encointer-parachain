4cfa51c9c360a9921c224450304e757e3d10139d
Christian Langenbacher <clangenb+gh@protonmail.ch>
2022-06-04 10:02:46 +0200

    [service] fix benchmarks

--- polkadot-parachains/src/service.rs
+++ polkadot-parachains/src/service.rs
@@ -71,7 +71,7 @@ type HostFunctions =
 pub struct EncointerParachainRuntimeExecutor;
 
 impl sc_executor::NativeExecutionDispatch for EncointerParachainRuntimeExecutor {
-	type ExtendHostFunctions = ();
+	type ExtendHostFunctions = frame_benchmarking::benchmarking::HostFunctions;
 
 	fn dispatch(method: &str, data: &[u8]) -> Option<Vec<u8>> {
 		parachain_runtime::api::dispatch(method, data)
@@ -86,7 +86,7 @@ impl sc_executor::NativeExecutionDispatch for EncointerParachainRuntimeExecutor 
 pub struct LaunchParachainRuntimeExecutor;
 
 impl sc_executor::NativeExecutionDispatch for LaunchParachainRuntimeExecutor {
-	type ExtendHostFunctions = ();
+	type ExtendHostFunctions = frame_benchmarking::benchmarking::HostFunctions;
 
 	fn dispatch(method: &str, data: &[u8]) -> Option<Vec<u8>> {
 		launch_runtime::api::dispatch(method, data)
