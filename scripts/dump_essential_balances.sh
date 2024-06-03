e740c99a395bdae99acddaa0ca2f84597190c3a8
Alain Brenzikofer <alain@integritee.network>
2024-06-03 22:17:58 +0200

    github env file

--- scripts/dump_essential_balances.sh
+++ scripts/dump_essential_balances.sh
@@ -17,19 +17,18 @@ ROCOCO="ws://127.0.0.1:9999"
 
 # dummy treasury subkey inspect --public 0x0000000000000000000000000000000000000000000000000000000000000000
 TREASURY=5C4hrfjw9DjXZTzV3MwzrrAr9P1MJhSrvWGWqi1eSuyUpnhM
+POT=5EYCAe5cKPAoFh2HnQQvpKqRYZGqBpaA87u4Zzw89qPE58is
 
 function print_balances() {
     echo "*** print balances for $1"
-    echo "Rococo ROC"
-    polkadot-js-api --ws $ROCOCO query.system.account $1 | jq .account.data.free
+    # echo "Rococo ROC"
+    # polkadot-js-api --ws $ROCOCO query.system.account $1 | jq .account.data.free
     echo "Encointer ROC"
-    polkadot-js-api --ws $ENCOINTER query.assets.account 0 $1 | jq .account.balance
+    polkadot-js-api --ws $ENCOINTER query.system.account $1 | jq .account.data.free
 }
 
 echo "*** total supply of ROC@Encointer"
 polkadot-js-api --ws $ENCOINTER query.balances.totalIssuance | jq .totalIssuance
-echo "*** ROC Treasury@Encointer balance"
-polkadot-js-api --ws $ENCOINTER query.assets.account 0 $TREASURY | jq .account.balance
 
 echo "*** Alice"
 print_balances 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY
@@ -39,6 +38,9 @@ echo "*** author1 "
 print_balances EXHoFmksfPoyTyuFgcJ2c11uLj9KPgCZ2wWxshxuDrvpZuq
 echo "*** author2 "
 print_balances E8Np37TgMvVNST2Qj7YpEMvNon2kBSPNSkMM9D2TuKjwKQZ
+echo "*** staking pot"
+print_balances $POT
+
 
 
 
